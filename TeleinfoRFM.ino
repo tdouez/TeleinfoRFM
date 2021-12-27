7adbfbf25be8ebb5ed3fb2eddfb14844407ce836
FuméeBleue <mail@fumeebleue.fr>
2021-12-27 14:51:51 +0100

    V2.0.1
    Fix bug aléatoire, blocage envoi data

--- TeleinfoRFM.ino
+++ TeleinfoRFM.ino
@@ -35,6 +35,7 @@
 // 2021/07/01 - FB V1.0.2 - Add ETIQU_URMS1..3
 // 2021/11/21 - FB V1.0.3 - Bug fix  modes triphasé et producteur
 // 2021/12/08 - FB V2.0.0 - Utilisation de la librairie libTeleinfo (https://github.com/hallard/LibTeleinfo) et chg protocole vers GW
+// 2021/12/27 - FB V2.0.1 - Fix bug aléatoire, blocage envoi data
 //--------------------------------------------------------------------
 
 #define TI_DEBUG
@@ -45,7 +46,7 @@
 #include <jled.h>
 #include <LibTeleinfo.h>
 
-#define VERSION   "v2.0.0"
+#define VERSION   "v2.0.1"
 
 #define MODE_TIC  TINFO_MODE_STANDARD    // TINFO_MODE_HISTORIQUE ou TINFO_MODE_STANDARD
 
@@ -61,9 +62,10 @@
 // Seuil : 4,5v à l'entrée du régulateur 3,3v > 4,5v*100k/(100k+68k)=2,68v sur A0
 //                                                 3,3v->1023 => 2,68->830
 //-------------------------------------------------------------------------------------
-#define SEUIL_CHARGE   820
+#define SEUIL_CHARGE_FULL   825
+#define SEUIL_CHARGE_98     810
 
-#define RFM_TX_POWER   8   // 5..23 dBm, ne pas dépasser les 9db car la capacité du supercondo (2x0,47F) ne serait pas suffisante. l'arduino rebooterait en permanence
+#define RFM_TX_POWER   10   // 5..23 dB
 
 #define LG_TRAME_MAX  125
 #define LG_TRAME_MIN  5
@@ -72,14 +74,13 @@
 
 
 boolean full_tic = true;
-boolean flag_tic = true;
+boolean flag_send_tic = true;
 boolean flag_adresse_tic;
 boolean flag_boot;
 boolean flag_tic_histo;
 
 unsigned long SEND_FREQUENCY_FULL = 600000; // 10mn, Minimum time between send (in milliseconds). 
 unsigned long SEND_FREQUENCY_TIC  =  15000; // 15s,  Minimum time between send (in milliseconds). 
-unsigned long TIMEOUT_SERIAL_TIC  =  10000; // 10s,  Minimum time between send (in milliseconds). 
 unsigned long lastTime_full = 0;
 unsigned long lastTime_tic = 0;
 unsigned long lastTime_timeout_tic = 0;
@@ -90,6 +91,18 @@ char adresse_compteur[16];
 auto led_chg = JLed(CHARGE_LED_PIN);
 TInfo tinfo;
 
+
+// ---------------------------------------------------------------- 
+// change_etat_led_teleinfo
+// ---------------------------------------------------------------- 
+void change_etat_led_teleinfo()
+{
+  static int led_state;
+
+  led_state = !led_state;
+  digitalWrite(TELEINFO_LED_PIN, led_state);
+
+}
 
 // ---------------------------------------------------------------- 
 // LoRa_sendMessage
@@ -124,7 +137,7 @@ char mode[5];
   strcat(trame, VERSION); 
   strcat(trame, ";");
   Serial.println(trame);
-  verif_charge_condo();
+  verif_charge_condo(SEUIL_CHARGE_98);
   LoRa_sendMessage(trame);
 
   Serial.println(F(">> fin Send BOOT"));
@@ -134,30 +147,27 @@ char mode[5];
 // search_adress_teleinfo
 //    Envoi trame de teleinfo
 // ---------------------------------------------------------------- 
-boolean search_adress_teleinfo(ValueList *vl_tic, boolean all_tic)
+boolean search_adress_teleinfo(ValueList *vl_tic)
 {
 
-  if (vl_tic && flag_tic) {
+  if (vl_tic) {
     // parcours liste chainée vl_tic
     while (vl_tic->next) {
       vl_tic = vl_tic->next;
-      // uniquement sur les nouvelles valeurs ou celles modifiées ou toutes
-      if ( all_tic || ( vl_tic->flags & (TINFO_FLAGS_UPDATED | TINFO_FLAGS_ADDED) )) {
-        digitalWrite(TELEINFO_LED_PIN, HIGH);
-        if (vl_tic->name && strlen(vl_tic->name) && vl_tic->value && strlen(vl_tic->value)) {
-          if (strstr(vl_tic->name, "ADCO") || strstr(vl_tic->name, "ADSC")) {
-            strncpy(adresse_compteur, vl_tic->value, 12);
-            Serial.print(F("Adresse compteur: "));
-            Serial.println(adresse_compteur);
-            flag_adresse_tic = true;
-            break;
-          }
-        }
-        digitalWrite(TELEINFO_LED_PIN, LOW);
+
+      if (vl_tic->name && strlen(vl_tic->name) && vl_tic->value && strlen(vl_tic->value)) {
+        change_etat_led_teleinfo();
+        if (strstr(vl_tic->name, "ADCO") || strstr(vl_tic->name, "ADSC")) {
+          strncpy(adresse_compteur, vl_tic->value, 12);
+          Serial.print(F("Adresse compteur: "));
+          Serial.println(adresse_compteur);
+          flag_adresse_tic = true;
+          break;
+         }
       }
     }
   }
-  
+  digitalWrite(TELEINFO_LED_PIN, LOW);
 }
 
 // ---------------------------------------------------------------- 
@@ -170,16 +180,16 @@ char trame[150];
 boolean flag_SINSTS = false;
 
   
-  if (vl_tic && flag_tic) {
-
+  if (vl_tic) {
     // parcours liste chainée vl_tic
     while (vl_tic->next) {
       vl_tic = vl_tic->next;
+      
       // uniquement sur les nouvelles valeurs ou celles modifiées ou toutes
       if ( all_tic || ( vl_tic->flags & (TINFO_FLAGS_UPDATED | TINFO_FLAGS_ADDED) )) {
-        digitalWrite(TELEINFO_LED_PIN, HIGH);
+     
         if (vl_tic->name && strlen(vl_tic->name) > 2 && vl_tic->value && strlen(vl_tic->value) > 1) {
-
+          change_etat_led_teleinfo();
           // entete trame --------
           trame[0] = 0;
           strcat(trame, ENTETE);
@@ -194,25 +204,28 @@ boolean flag_SINSTS = false;
           
           strcat(trame, vl_tic->name);
           strcat(trame, ";");
-          strncat(trame, vl_tic->value, 32);  // limite à 32 caractères pas valeur
+          strncat(trame, vl_tic->value, 32);  // limite à 32 caractères par valeur
           strcat(trame, ";");
           
           Serial.println(trame);
-          verif_charge_condo();
-          LoRa_sendMessage(trame);
-        }
-        digitalWrite(TELEINFO_LED_PIN, LOW);
+
+          if (flag_send_tic) {
+            verif_charge_condo(SEUIL_CHARGE_98);
+            LoRa_sendMessage(trame);   
+          } 
+        } 
       }
     }
-    flag_tic = false;
+    flag_send_tic = false;
   }
+  digitalWrite(TELEINFO_LED_PIN, LOW);
 }
 
 // ---------------------------------------------------------------- 
 // charge_condo
 //    charge le super condo avec gestion led 
 // ---------------------------------------------------------------- 
-bool charge_condo()
+bool charge_condo(int ref_seuil)
 {
 boolean flag_cap = false;
 
@@ -220,7 +233,7 @@ boolean flag_cap = false;
   led_chg.Breathe(1000).DelayAfter(500).Forever();
 
   while (flag_cap == false) {
-    if (analogRead(A0) >= SEUIL_CHARGE) flag_cap = true;
+    if (analogRead(A0) >= ref_seuil) flag_cap = true;
     led_chg.Update();
   }
   led_chg.Off();
@@ -232,13 +245,13 @@ boolean flag_cap = false;
 //    test niveau tension sur le super condo. 
 //    retourne vrai si niveau suffisant
 // ---------------------------------------------------------------- 
-boolean test_charge_condo()
+boolean test_charge_condo(int ref_seuil)
 {
 boolean rc = true;
 int seuil;
 
   seuil = analogRead(A0);
-  if (seuil < SEUIL_CHARGE) {
+  if (seuil < ref_seuil) {
     rc=false;
   }
   return rc;
@@ -249,9 +262,9 @@ int seuil;
 //    verif niveau tension sur le super condo
 //    lance la charge si niveau insuffisant
 // ---------------------------------------------------------------- 
-void verif_charge_condo()
+void verif_charge_condo(int ref_seuil)
 {
-  if (test_charge_condo() == false) charge_condo();  
+  if (test_charge_condo(ref_seuil) == false) charge_condo(ref_seuil);  
 }
 
 /* ======================================================================
@@ -264,11 +277,10 @@ Comments: -
 void NewFrame(ValueList *vl_tic)
 {
 
-  if (!flag_adresse_tic) search_adress_teleinfo(vl_tic, full_tic);
-    else send_teleinfo(vl_tic, full_tic);
+  if (!flag_adresse_tic) search_adress_teleinfo(vl_tic);
+    else send_teleinfo(vl_tic, true);
     
-  if (full_tic) full_tic = false;
-  
+  full_tic = false;
 }
 
 /* ======================================================================
@@ -282,10 +294,10 @@ Comments: it's called only if one data in the frame is different than
 void UpdatedFrame(ValueList *vl_tic)
 {
 
-  if (!flag_adresse_tic) search_adress_teleinfo(vl_tic, full_tic);
+  if (!flag_adresse_tic) search_adress_teleinfo(vl_tic);
     else send_teleinfo(vl_tic, full_tic);
     
-  if (full_tic) full_tic = false;
+  full_tic = false;
 }
 
 // ---------------------------------------------------------------- 
@@ -322,7 +334,8 @@ void setup()
   // init interface série suivant mode TIC
   Serial.flush();
   Serial.end();
-  Serial.begin(MODE_TIC == TINFO_MODE_HISTORIQUE ? 1200 : 9600, SERIAL_7E1);
+ 
+  Serial.begin(MODE_TIC == TINFO_MODE_HISTORIQUE ? 1200 : 9600);
 
   Serial.println(F("   __|              _/           _ )  |"));
   Serial.println(F("   _| |  |   ` \\    -_)   -_)    _ \\  |   -_)  |  |   -_)"));
@@ -331,14 +344,14 @@ void setup()
   Serial.println(VERSION);
 
   // première charge super condo
-  verif_charge_condo();
+  verif_charge_condo(SEUIL_CHARGE_FULL);
 
   // activation alim et init RFM
   digitalWrite(POWER_PIN, HIGH);
   Serial.print(F("Init RF95: "));
   while (!LoRa.begin(868E6)) {
     Serial.println(F("Starting LoRa failed!"));
-    charge_condo();
+    charge_condo(SEUIL_CHARGE_FULL);
   }
   LoRa.enableCrc();
   LoRa.setTxPower(RFM_TX_POWER);
@@ -364,7 +377,7 @@ void loop()
 {
   uint32_t currentTime = millis();
 
-  if (Serial.available()) tinfo.process(Serial.read());
+  if (Serial.available()) tinfo.process(Serial.read() & 127);
 
   if (flag_adresse_tic) {
   
@@ -378,13 +391,14 @@ void loop()
     if (currentTime - lastTime_full > SEND_FREQUENCY_FULL) {
       Serial.println(F("SEND_FREQUENCY_FULL"));
       full_tic = true;
+      flag_send_tic = true;
       lastTime_full = currentTime;
     }
   
     // envoyer les données tous les SEND_FREQUENCY_TIC ms
     if (currentTime - lastTime_tic > SEND_FREQUENCY_TIC) {
       Serial.println(F("SEND_FREQUENCY_TIC"));
-      flag_tic = true;
+      flag_send_tic = true;
       lastTime_tic = currentTime;
     }
   }
