71a011d04d55c1a5d54e890580dd038bb967cce2
FuméeBleue <mail@fumeebleue.fr>
2022-01-08 18:25:26 +0100

    v2.02
    Optimisation utilisation mémoire et emissions datas. Ajout détection vitesse TIC

--- TeleinfoRFM.ino
+++ TeleinfoRFM.ino
@@ -36,17 +36,22 @@
 // 2021/11/21 - FB V1.0.3 - Bug fix  modes triphasé et producteur
 // 2021/12/08 - FB V2.0.0 - Utilisation de la librairie libTeleinfo (https://github.com/hallard/LibTeleinfo) et chg protocole vers GW
 // 2021/12/27 - FB V2.0.1 - Fix bug aléatoire, blocage envoi data
+// 2021/01/07 - FB V2.0.2 - Optimisation utilisation mémoire et emissions datas. Ajout détection vitesse TIC.
 //--------------------------------------------------------------------
 
 #include <Arduino.h>
 #include <SPI.h>
 #include <LoRa.h>
-#include <jled.h>
 #include <LibTeleinfo.h>
 
-#define VERSION   "v2.0.1"
+#ifdef __arm__
+// should use uinstd.h to define sbrk but Due causes a conflict
+extern "C" char* sbrk(int incr);
+#else  // __ARM__
+extern char *__brkval;
+#endif  // __arm__
 
-#define MODE_TIC  TINFO_MODE_STANDARD    // TINFO_MODE_HISTORIQUE ou TINFO_MODE_STANDARD
+#define VERSION   "v2.0.2"
 
 #define ENTETE  "$"
 
@@ -64,20 +69,43 @@
 #define RFM_TX_POWER   10   // 5..23 dB
 
 boolean full_tic = true;
+boolean flag_tic = true;
 boolean flag_send_tic = true;
 boolean flag_adresse_tic;
 boolean flag_boot;
-boolean flag_tic_histo;
 
-unsigned long SEND_FREQUENCY_FULL = 600000; // 10mn, Minimum time between send (in milliseconds). 
+unsigned long SEND_FREQUENCY_FULL = 300000; // 5mn, Minimum time between send (in milliseconds). 
 unsigned long SEND_FREQUENCY_TIC  =  15000; // 15s,  Minimum time between send (in milliseconds). 
 unsigned long lastTime_full = 0;
 unsigned long lastTime_tic = 0;
-unsigned long lastTime_timeout_tic = 0;
+//unsigned long lastTime_timeout_tic = 0;
 char adresse_compteur[13];
+unsigned int mode_tic;
 
-auto led_chg = JLed(CHARGE_LED_PIN);
 TInfo tinfo;
+
+
+// ---------------------------------------------------------------- 
+// freeMemory
+// ---------------------------------------------------------------- 
+int freeMemory() {
+  char top;
+#ifdef __arm__
+  return &top - reinterpret_cast<char*>(sbrk(0));
+#elif defined(CORE_TEENSY) || (ARDUINO > 103 && ARDUINO != 151)
+  return &top - __brkval;
+#else  // __arm__
+  return __brkval ? &top - __brkval : &top - __malloc_heap_start;
+#endif  // __arm__
+}
+
+// ---------------------------------------------------------------- 
+// freeMemory
+// ---------------------------------------------------------------- 
+void affiche_freeMemory() {
+	Serial.print(F("SRAM:"));
+	Serial.println(freeMemory());
+}
 
 
 // ---------------------------------------------------------------- 
@@ -93,12 +121,75 @@ void change_etat_led_teleinfo()
 }
 
 // ---------------------------------------------------------------- 
+// change_etat_led_teleinfo
+// ---------------------------------------------------------------- 
+void clignote_led(uint8_t led, uint8_t nbr, uint8_t delais)
+{
+int led_state;
+
+  for (int i=0; i<nbr*2; i++) {
+    led_state = !led_state;
+    digitalWrite(led, led_state);
+    delay(delais);
+  }
+  digitalWrite(led, LOW);
+}
+
+// ---------------------------------------------------------------- 
+// init_speed_TIC
+// ---------------------------------------------------------------- 
+unsigned int init_speed_TIC()
+{
+boolean flag_timeout = false;
+boolean flag_found_speed = false;
+boolean flag_debut_trame = false;
+boolean flag_milieu_trame = false;
+boolean flag_fin_trame = false;
+uint32_t currentTime = millis();
+unsigned int mode;
+
+  digitalWrite(TELEINFO_LED_PIN, HIGH);
+  digitalWrite(CHARGE_LED_PIN, HIGH);
+  
+  // Test en mode historique
+  // Recherche des éléments de début, milieu et fin de trame 
+  Serial.begin(1200); // mode historique
+  while (!flag_timeout && !flag_found_speed) {
+    if (Serial.available()>0) {
+      char in = (char)Serial.read() & 127;  // seulement sur 7 bits
+      if (in == 0x0A) flag_debut_trame = true; // début trame
+      if (in == 0x09) flag_milieu_trame = true; // milieu trame
+      if (in == 0x0D) flag_fin_trame = true; // fin trame
+
+      if (flag_debut_trame && flag_milieu_trame && flag_fin_trame) flag_found_speed = true;
+      if (currentTime + 10000 <  millis()) flag_timeout = true; // 10s de timeout
+    }
+  }
+
+  if (flag_timeout) { // trame avec vistesse histo non trouvée donc passage en mode standard
+     mode = TINFO_MODE_STANDARD;
+     Serial.end();
+     Serial.begin(9600); // mode standard
+     Serial.println(F("TIC mode standard"));
+     clignote_led(CHARGE_LED_PIN, 3, 500);
+  }
+  else {
+    mode = TINFO_MODE_HISTORIQUE;
+    Serial.println(F("TIC mode historique"));
+    clignote_led(CHARGE_LED_PIN, 5, 500);
+  }
+  
+  digitalWrite(TELEINFO_LED_PIN, LOW);
+
+  return mode;
+}
+
+// ---------------------------------------------------------------- 
 // send_boot
 //    Envoi info au démarrge avec version
 // ---------------------------------------------------------------- 
 void send_boot() 
 {
-char mode[5];
  
   //Serial.println(F(">> Send BOOT"));
 
@@ -122,7 +213,7 @@ char mode[5];
 // search_adress_teleinfo
 //    Envoi trame de teleinfo
 // ---------------------------------------------------------------- 
-boolean search_adress_teleinfo(ValueList *vl_tic)
+void search_adress_teleinfo(ValueList *vl_tic)
 {
 
   if (vl_tic) {
@@ -149,18 +240,17 @@ boolean search_adress_teleinfo(ValueList *vl_tic)
 // charge_condo
 //    charge le super condo avec gestion led 
 // ---------------------------------------------------------------- 
-bool charge_condo(int ref_seuil)
+void charge_condo(int ref_seuil)
 {
 boolean flag_cap = false;
 
   Serial.print(F(">> Charge Condo.."));
-  led_chg.Breathe(1000).DelayAfter(500).Forever();
+  digitalWrite(CHARGE_LED_PIN, HIGH);
 
   while (flag_cap == false) {
     if (analogRead(A0) >= ref_seuil) flag_cap = true;
-    led_chg.Update();
   }
-  led_chg.Off();
+  digitalWrite(CHARGE_LED_PIN, LOW);
   Serial.println(F(". done"));
 }
 
@@ -189,6 +279,80 @@ int seuil;
 void verif_charge_condo(int ref_seuil)
 {
   if (test_charge_condo(ref_seuil) == false) charge_condo(ref_seuil);  
+}
+
+// ---------------------------------------------------------------- 
+// send_teleinfo
+//    Envoi trame de teleinfo
+// ---------------------------------------------------------------- 
+void send_teleinfo(ValueList *vl_tic, boolean all_tic)
+{
+  
+  if (vl_tic && flag_tic) {
+
+    // parcours liste chainée vl_tic
+    while (vl_tic->next) {
+      vl_tic = vl_tic->next;
+      // uniquement sur les nouvelles valeurs ou celles modifiées ou toutes
+      if ( all_tic || ( vl_tic->flags & (TINFO_FLAGS_UPDATED | TINFO_FLAGS_ADDED) )) {
+        digitalWrite(TELEINFO_LED_PIN, HIGH);
+        if (vl_tic->name && strlen(vl_tic->name) > 2 && vl_tic->value && strlen(vl_tic->value) > 1) {
+		  
+		  verif_charge_condo(SEUIL_CHARGE_98);
+		  LoRa.idle();
+		  LoRa.beginPacket();    // start packet
+		  LoRa.print(ENTETE);  
+		  LoRa.print(F("Linky_")); 
+		  LoRa.print(adresse_compteur); 
+		  LoRa.print(F(";"));
+		  LoRa.print(vl_tic->name); 
+		  LoRa.print(F(";"));
+		  LoRa.print(vl_tic->value); 
+		  LoRa.print(F(";"));
+		  LoRa.endPacket();      // finish packet and send it
+		  LoRa.sleep();
+        }
+        digitalWrite(TELEINFO_LED_PIN, LOW);
+      }
+    }
+    flag_tic = false;
+  }
+}
+
+/* ======================================================================
+Function: NewFrame 
+Purpose : callback when we received a complete teleinfo frame
+Input   : linked list pointer on the concerned data
+Output  : - 
+Comments: -
+====================================================================== */
+void NewFrame(ValueList *vl_tic)
+{
+  affiche_freeMemory();
+  
+  if (!flag_adresse_tic) search_adress_teleinfo(vl_tic);
+    else send_teleinfo(vl_tic, full_tic);
+    
+  if (full_tic) full_tic = false;
+  
+}
+
+/* ======================================================================
+Function: NewFrame 
+Purpose : callback when we received a complete teleinfo frame
+Input   : linked list pointer on the concerned data
+Output  : - 
+Comments: it's called only if one data in the frame is different than
+          the previous frame
+====================================================================== */
+void UpdatedFrame(ValueList *vl_tic)
+{
+  affiche_freeMemory();
+  
+  if (!flag_adresse_tic) search_adress_teleinfo(vl_tic);
+    else send_teleinfo(vl_tic, full_tic);
+    
+  if (full_tic) full_tic = false;
 }
 
 /* ======================================================================
@@ -264,13 +428,10 @@ void setup()
   digitalWrite(POWER_PIN, LOW);
   digitalWrite(TELEINFO_LED_PIN, LOW);
   digitalWrite(CHARGE_LED_PIN, LOW);
-  led_chg.Off();
 
   // init interface série suivant mode TIC
-  //Serial.flush();
-  //Serial.end();
- 
-  Serial.begin(MODE_TIC == TINFO_MODE_HISTORIQUE ? 1200 : 9600);
+  mode_tic = init_speed_TIC();
+  //Serial.begin(MODE_TIC == TINFO_MODE_HISTORIQUE ? 1200 : 9600);
 
   Serial.println(F("   __|              _/           _ )  |"));
   Serial.println(F("   _| |  |   ` \\    -_)   -_)    _ \\  |   -_)  |  |   -_)"));
@@ -297,10 +458,12 @@ void setup()
   flag_adresse_tic = false;
   
   // init interface TIC
-  tinfo.init(MODE_TIC);
-  tinfo.attachData(DataCallback);
+  tinfo.init(mode_tic);
+  //tinfo.attachData(DataCallback);
+  tinfo.attachNewFrame(NewFrame);
+  tinfo.attachUpdatedFrame(UpdatedFrame);
 
-  Serial.println(F("Fin setup"));
+  affiche_freeMemory();
 }
 
 
@@ -309,5 +472,32 @@ void setup()
 // ---------------------------------------------------------------- 
 void loop()
 {
+uint32_t currentTime = millis();
+
   if (Serial.available()) tinfo.process(Serial.read());
+  
+  if (flag_adresse_tic) {
+	  
+    if (flag_boot) {
+      // envoi version au démarrage
+      send_boot();
+      flag_boot = false;
+    }
+    
+    // envoyer l'ensemble des données tous les SEND_FREQUENCY_FULL ms
+    if (currentTime - lastTime_full > SEND_FREQUENCY_FULL) {
+      Serial.println(F("SEND_FREQUENCY_FULL"));
+      full_tic = true;
+      flag_tic = true;
+      lastTime_full = currentTime;
+    }
+  
+    // envoyer les données tous les SEND_FREQUENCY_TIC ms
+    if (currentTime - lastTime_tic > SEND_FREQUENCY_TIC) {
+      Serial.println(F("SEND_FREQUENCY_TIC"));
+      flag_tic = true;
+      lastTime_tic = currentTime;
+    }
+  }
+
 }
